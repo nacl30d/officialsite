9c3619dd5c023946d1d9666ccd0a8a16cd0c9397
Daiki Shiozawa <me@nacl30d.com>
2025-12-30 16:49:45 +0900

    feat: Upgrade to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [],
 });
